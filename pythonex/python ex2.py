388515e0e3140feee0abf1b41f09b437fb5fd302
liang <liangguiyu0807@163.com>
2022-03-04 01:12:43 +0800

    变量及数据类型

--- pythonex/python ex2.py
+++ pythonex/python ex2.py
@@ -6,9 +6,28 @@
 # @Software : PyCharm
 import keyword
 
-print('生活','生存')
+print('生活', '生存')
 #  查看关键字 有哪些
 print(keyword.kwlist)
 
+print('........................................')
 
-
+# 变量的命名变更
+name = 6
+print(name)
+#  type 可以看到是什么类型的变量
+a = 5
+print(type(a))
+a = '二百五'
+print(type(a))
+# 给变量 进行赋值的过程实际上是赋予内存地址 ID（） 变量改变时重新 也会重新变更内存地址
+a = 10
+print(id(a))
+a = 100
+print(id(a))
+# 变量赋值给变量  A的存放地址就赋予了B
+a = 10
+b = a
+print(id(a))
+print(id(a))
+# 数据类型 基本的有如下
