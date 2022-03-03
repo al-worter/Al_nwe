58d114798d49807f5d17a90878f3ad10282dbf8d
liang <liangguiyu0807@163.com>
2022-03-04 00:10:43 +0800

    exercise1   练习2  huawei 电脑

---
+++ pythonex/python ex2.py
@@ -0,0 +1,14 @@
+# _*_ coding: UTF-8 _*_
+# @Time     : 2022/3/4 上午 12:28
+# @Author   : liang yuyu
+# @Site     : http://www.cdtest.cn/
+# @File     : python ex2.py
+# @Software : PyCharm
+import keyword
+
+print('生活','生存')
+#  查看关键字 有哪些
+print(keyword.kwlist)
+
+
+
