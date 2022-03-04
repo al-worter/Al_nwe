7d2df98a1038e55718b74ab3e54413aee02ebbb5
liang <liangguiyu0807@163.com>
2022-03-04 16:09:41 +0800

    3/4

---
+++ pythonex3/orperater1.py
@@ -0,0 +1,11 @@
+# _*_ coding: UTF-8 _*_
+# @Time     : 2022/3/4 下午 04:05
+# @Author   : liang yuyu
+# @Site     : http://www.cdtest.cn/
+# @File     : orperater1.py
+# @Software : PyCharm
+
+# 运算符
+# 分为六种： 算数运算符、比较（关系）运算符、逻辑运算符、赋值运算符、位和位
+print(5 / 2)
+print(6 / 2.2)
