975927dd68c22098f642da8610ef71235fb672e5
liang <liangguiyu0807@163.com>
2022-03-04 17:30:11 +0800

    运算符

--- pythonex3/orperater1.py
+++ pythonex3/orperater1.py
@@ -5,7 +5,21 @@
 # @File     : orperater1.py
 # @Software : PyCharm
 
-# 运算符
+# 算数运算符
 # 分为六种： 算数运算符、比较（关系）运算符、逻辑运算符、赋值运算符、位和位
 print(5 / 2)
 print(6 / 2.2)
+print(5 % 2) # 会取余数 1
+print(5/3) # 会除尽 ，四舍五入
+print(5 ** 2) # a ** b ,是a的b次方
+print(5//3) # 整除，不会除尽。
+
+# 比较运算符 ：运算之后两边比较的数据，得到布尔值 false true
+print(2 == 3)
+print(2 != 3)
+
+# 逻辑运算符
+# 两边都是布尔值，得到的上也是
+# and 与 ；or 或 ； not 非
+a = True
+b = False
